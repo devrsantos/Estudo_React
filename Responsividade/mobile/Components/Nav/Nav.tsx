6007f28e48f95e0a80f3c5c83d72759163f2cfc0
Renan Augusto dos Santos <devrsantos@gmail.com>
2022-11-16 16:52:23 -0300

    Adicionando TypeScript no Projeto

---
+++ Responsividade/mobile/Components/Nav/Nav.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from "react";
+
+const Nav = () => {
+  return (
+        <nav>
+          <p>
+              Schedule<span>Mail</span>
+            </p>
+        </nav>
+  )
+}
